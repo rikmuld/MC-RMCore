3ed569178e82537caa980bcb8c9fc8491ccb2ce1
rikmuld <rikmuld@gmail.com>
2018-02-01 01:19:17 +0900

    added typews

--- README.md
+++
@@ -1,2 +0,0 @@
-RikMuldsCore-mcmod
-==================
--- scala/com/rikmuld/corerm/gui/slots/SlotNoPickup.scala
+++
@@ -1,9 +0,0 @@
-package com.rikmuld.corerm.gui.slots
-
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.inventory.Slot
-
-trait SlotNoPickup extends Slot {
-  override def canTakeStack(par1EntityPlayer: EntityPlayer): Boolean =
-    false
-}
